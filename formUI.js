da25e06bb89701f56161e3fbe62bd5fdcea97cfd
MariuswebDev <efuetlanchamarius@gmail.com>
2026-06-06 21:40:06 +0100

    UI-Form Accessibility

---
+++ formUI.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.getElementById('toggleButton');
+const toggleBtnConfirm = document.getElementById('toggle-Confirm');
+const input = document.getElementById('password');
+const confirmInput = document.getElementById('confirm-password');
+
+toggleBtn.addEventListener('click', () => {
+    input.type = input.type === 'password' ? 'text' : 'password';
+})
+
+toggleBtnConfirm.addEventListener('click', () => {
+    confirmInput.type = confirmInput.type === 'password' ? 'text' : 'password';
+
+})
